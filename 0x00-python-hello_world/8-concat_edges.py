c2d52278e3c0669470fdba8349166fe94ef77bb8
root <halimadekkani9@gmain.com>
2023-06-05 18:51:55 -0700

    concat

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is interpretive, interactive and object-oriented programming.
+ which combines a remarkable power with a very clear syntax."
+str = str[39:66] + str[106:112] + str[:6]
+print(str)

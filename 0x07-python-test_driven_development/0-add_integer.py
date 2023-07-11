2fc15a92b765980109527d38ccda30b46493ea23
root <halimadekkani9@gmain.com>
2023-07-11 15:46:07 -0700

    add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
+def add_integer(a, b):
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):

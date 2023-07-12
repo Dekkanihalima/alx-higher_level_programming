e1c8193c4d1a4b7f5ad283fbb721b69c5e278847
root <halimadekkani9@gmain.com>
2023-07-11 18:34:33 -0700

    c python

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,28 +1,36 @@
 #include "Python.h"
+#include <stdio.h>
+
+void print_python_string(PyObject *p);
 
 /**
- * print_python_string - Prints information about Python strings.
- * @p: A PyObject string object.
+ * print_python_string - Checks if the object is a Python bytes object. If so
+ * print the size, attempt to print it as a string and the first 10 bytes at
+ * most in hexadecimal.
+ *
+ * @p: Pointer to a Python object.
+ *
+ * Return: void.
  */
 void print_python_string(PyObject *p)
 {
-	long int length;
-
-	fflush(stdout);
+	Py_ssize_t p_len;
+	char *str;
 
 	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	if (PyUnicode_Check(p))
+	{
+		p_len = PyUnicode_GET_LENGTH(p);
+		str = PyBytes_AsString(PyUnicode_AsUTF8String(p));
+		if (PyUnicode_IS_COMPACT_ASCII(p))
+			printf("  type: compact ascii\n");
+		else
+			printf("  type: compact unicode object\n");
+		printf("  length: %zd\n  value: %s\n", p_len, str);
+	}
+	else
 	{
 		printf("  [ERROR] Invalid String Object\n");
-		return;
 	}
-
-	length = ((PyASCIIObject *)(p))->length;
-
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+	fflush(stdout);
 }

91deee0413e626caacfffea142623f26bc43ae4d
root <halimadekkani9@gmain.com>
2023-07-12 17:05:35 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,10 @@
 
 
 def read_file(filename=""):
+    """ read-file print the file content
+    Args:
+        filename (path): file path
+
+    """
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")

d6cc781744e27c34bac99f215bcc877257f48a18
halimadekkani <halimadekkani9@gmail.com>
2023-09-12 16:23:50 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" function that writes a string to a text file (UTF8)  """
+""" 
+Write a function that writes a string to a text file (UTF8) and returns the number of characters written:
+
+"""
 
 def write_file(filename="", text=""):
     """ function that writes a string to a text file (UTF8)  """

fb4084efba4c0e9efb4cc0eac4c8e9b587150cd9
halimadekkani <halimadekkani9@gmail.com>
2023-09-12 16:47:17 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,8 +2,6 @@
 """ Write a function that writes a string to a text file (UTF8) and returns the number of characters written:
 """
 def write_file(filename="", text=""):
-    """ function that writes a string to a text file (UTF8)  """
-
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
         return len(text)

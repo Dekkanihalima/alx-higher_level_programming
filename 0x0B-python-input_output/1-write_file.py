d336c18b64d92af5bf0c8ae0b71d39828e21f96c
halimadekkani <halimadekkani9@gmail.com>
2023-09-12 16:26:44 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,6 +7,6 @@ Write a function that writes a string to a text file (UTF8) and returns the numb
 def write_file(filename="", text=""):
     """ function that writes a string to a text file (UTF8)  """
 
-    with open(filename, "w", encoding='utf-8') as f:
+    with open(filename, "a", encoding='utf-8') as f:
         return f.write(text)
 

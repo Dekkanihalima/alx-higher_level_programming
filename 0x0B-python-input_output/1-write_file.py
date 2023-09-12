7cc55c5e5a94258eed5594ffb0baca27904f9637
halimadekkani <halimadekkani9@gmail.com>
2023-09-12 16:40:01 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,6 @@ def write_file(filename="", text=""):
     """ function that writes a string to a text file (UTF8)  """
 
     with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+        f.write(text)
+        return len(text)
 

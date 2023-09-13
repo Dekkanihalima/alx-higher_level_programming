846209eaa8feed43091010f29790a7dae42d4fe9
halimadekkani <halimadekkani9@gmail.com>
2023-09-12 17:08:50 -0700

    task12:

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+create a functionthat returns a list of lists of integers representing the Pascal’s triangle of n:
+"""
+def pascal_triangle(n):
+    """ Pascal Triangle """
+    li = []
+    if n <= 0:
+        return li
+    for r in range(n):
+        for c in range(r + 1):
+            if c == 0:
+                li.append([1])
+            elif c == r:
+                li[r].append(1)
+            else:
+                li[r].append(li[r - 1][c] + li[r - 1][c - 1])
+    return li
